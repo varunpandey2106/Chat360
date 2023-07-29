70a175097cd9b5ba85c813d85dc5a984187bdfcb
varunpandey2106 <varun.pandey2106@gmail.com>
2023-07-30 00:13:14 +0530

    initial routes setup

--- ChatBot360/views.py
+++ ChatBot360/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+def chatbot(request):
+    return render(request, 'ChatBot360/chatbot.html')
