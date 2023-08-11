d352d143c87fef39d8f24dd9d3a6d320cb4f6792
varunpandey2106 <varun.pandey2106@gmail.com>
2023-08-11 23:19:59 +0530

    asgi reconfig

--- Chat360/asgi.py
+++ Chat360/asgi.py
@@ -1,29 +1,22 @@
-
-
 import os
-
-from channels.auth import AuthMiddlewareStack
+from channels.auth import AuthMiddlewareStack  
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
-
+from Text360 import routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Chat360.settings')
 
-from Text360 import routing
-
 django_asgi_application = get_asgi_application()
 
-application=ProtocolTypeRouter(
-    {
-        'http': 'django_asgi_application',
-        'websocket': AllowedHostsOriginValidator (
-            AuthMiddlewareStack(URLRouter(routing.websocket_urlpatterns))
-    
-
-        )                                              ,
-
-    }
-)
-
+application = ProtocolTypeRouter({
+    'http': django_asgi_application,
+    'websocket': AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(
+                routing.websocket_urlpatterns
+            )
+        )
+    ),
+})
 
