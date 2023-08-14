e3e1e265070f5c1a28df65d4b871040ac10db729
varunpandey2106 <varun.pandey2106@gmail.com>
2023-08-14 20:53:47 +0530

    event handler not being triggered

--- Text360/static/js/window.js
+++ Text360/static/js/window.js
@@ -1,95 +1,92 @@
-function getCookie(name){
-    var cookieValue=null
+function getCookie(name) {
+    var cookieValue = null;
 
-    if (document.cookie && document.cookie !=''){
-        var cookies= document.cookie.split(';')
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
 
-        for (var i=0; i<cookies.length;i++){
-            var cookie=cookies[i].trim()
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = cookies[i].trim();
 
-            if(cookie.substring(0,name.length+1)==(name+ '=')){
-                cookieValue=decodeURIComponent(cookie.substring(name.length+1))
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
 
-                break
+                break;
             }
         }
     }
 
-    return cookieValue
+    return cookieValue;
 }
 
-function sendMessage(){
+function sendMessage(message) {
     chatSocket.send(JSON.stringify({
-        'type':'message',
-        'message': chatInputElement.value,
-        'name':chatName
-    }))
-
-    chatInputElement.value=''
+        'type': 'message',
+        'message': message,
+        'name': chatName
+    }));
 }
 
-function onChatMessage(){
-    console.log('onChatMessage', data)
+function onChatMessage(event) {
+    const data = JSON.parse(event.data);
 
-    if (data.type=='chat_message'){
-        if(data.agent){
+    console.log('onChatMessage', data);
 
-        }else{
-            chatLogElement.innerHTML+='<div>$(data.message}'
+    if (data.type === 'chat_message') {
+        if (data.agent) {
+            // Handle messages from agents if needed
+        } else {
+            const chatLogElement = document.querySelector("#chat_log");
+            const newMessageElement = document.createElement("div");
+            newMessageElement.textContent = data.message;
+            chatLogElement.appendChild(newMessageElement);
         }
     }
 }
 
-async function JoinChatRoom(chatJoinElement){
-    console.log('joinChatRoom')
+async function JoinChatRoom(chatJoinElement) {
+    console.log('joinChatRoom');
 
-    chatName= chatJoinElement.value
+    const chatName = chatJoinElement.value;
 
-    console.log("join as: ", chatName)
-    console.log("room uuid: ", chatRoomUuid)
+    console.log("join as: ", chatName);
+    console.log("room uuid: ", chatRoomUuid);
 
-    const data= new FormData()
-    data.append('name', chatName)
-    data.append("url", chatWindowUrl)
+    const data = new FormData();
+    data.append('name', chatName);
+    data.append("url", chatWindowUrl);
 
-    await fetch(`/features/Text360/create-room/${chatRoomUuid}/`, {
-        method: 'POST',
-        headers: {
-            'X-CSRFToken': getCookie('csrftoken')
-        },
-        body: data
-    })
-    .then(function (res) {       
-        return res.json()
-    })
-    .then(function (data) {
-        console.log('data', data)
-    });
-    
+    try {
+        const res = await fetch(`/features/Text360/create-room/${chatRoomUuid}/`, {
+            method: 'POST',
+            headers: {
+                'X-CSRFToken': getCookie('csrftoken')
+            },
+            body: data
+        });
+
+        const responseData = await res.json();
+        console.log('data', responseData);
+    } catch (error) {
+        console.error('Error:', error);
+    }
+
     chatSocket = new WebSocket(`ws://${window.location.host}/ws/${chatRoomUuid}/`);
 
-
-    chatSocket.onopen=function(e){
-        console.log('onOpen- chat socket was opened')
+    chatSocket.onopen = function (e) {
+        console.log('onOpen - chat socket was opened');
     }
 
-
-    chatSocket.onclose= function(e){
-        console.log('onClose- chat socket was closed')
+    chatSocket.onclose = function (e) {
+        console.log('onClose - chat socket was closed');
     }
 
+    chatSocket.onmessage = function (e) {
+        console.log('Received a message:', e.data);
+        onChatMessage(e);
+    };
+    
 
     
-    chatSocket.onmessage= function(e){
-       
-
-        onChatMessage(JSON.parse(e.data))
-
-        console.log('onMessage-message kia')
-
-       
-    }
-
 }
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -97,10 +94,11 @@ document.addEventListener("DOMContentLoaded", function () {
     const chatWelcomeElement = document.querySelector("#chat_welcome");
     const chatRoomElement = document.querySelector("#chat_room");
     const chatLogElement = document.querySelector("#chat_log");
+    const chatMessageSubmit = document.querySelector('#chat_message_submit')
 
     chatOpenElement.addEventListener("click", function (e) {
         e.preventDefault();
-        chatWelcomeElement.classList.remove('hidden'); // Show the "Enter Name" and "Join Chat" options
+        chatWelcomeElement.classList.remove('hidden');
     });
 
     const chatJoinElement = document.querySelector("#chat_join");
@@ -116,19 +114,14 @@ document.addEventListener("DOMContentLoaded", function () {
             return;
         }
 
-        // Show chat room, update welcome message, and set user's name
         chatRoomElement.classList.remove('hidden');
         chatLogElement.textContent = `Welcome, ${chatName}! Type a message and wait for an agent to join...`;
 
-        // Now, you can use JoinChatRoom() with chatJoinElement defined
         JoinChatRoom(chatJoinElement);
 
-        
-       
         return false;
     });
 
-    const chatMessageSubmit = document.querySelector("#chat_message_submit");
     chatMessageSubmit.addEventListener("click", function (e) {
         e.preventDefault();
 
@@ -138,6 +131,8 @@ document.addEventListener("DOMContentLoaded", function () {
         if (message === "") {
             return; // Do not send empty messages
         }
+
+        sendMessage(message);
 
         // Example: Display the message in the chat log
         const newMessageElement = document.createElement("div");
@@ -149,6 +144,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
         return false;
     });
+
 });
 
 // Define chatRoomUuid and chatWindowUrl with actual values
